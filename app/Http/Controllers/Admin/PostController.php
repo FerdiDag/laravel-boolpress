5e85b955cc146e049a0df71281373af3d5669803
Ferdinando <ferdi.dagostino@gmail.com>
2020-07-15 18:25:00 +0200

    aggiunto detatch alla destroy

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -156,6 +156,7 @@ class PostController extends Controller
     {
         $post = Post::find($id);
         if ($post) {
+            $post->tags()->detach();
             $post->delete();
             return redirect()->route('admin.posts.index');
         } else {
